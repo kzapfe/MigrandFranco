c9def3646501d4f98d1a7ed419fd41ecff88c907
karel <kzapfe@gmail.com>
2015-06-24 14:23:15 -0500

    regexp

---
+++ ExtractGoddamnedPositions01.jl
@@ -0,0 +1,8 @@
+function ExtractGoddamnedPositions01.jl(mergas::String)
+    turegexp=r"([0-9]{2})_(0-9]{2})"
+    parapa=match(turegexp,mergas)
+    perepe=match(turegexp,mergas,parapa.offset+5)
+    parte1=int(parapa.captures)
+    parte2=int(perepe.captures)
+    return (parte1, parte2)
+end
