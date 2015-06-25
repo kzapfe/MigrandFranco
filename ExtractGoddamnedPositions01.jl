618f21dd3ef070cc81967de79b33dee662642d34
karel <kzapfe@gmail.com>
2015-06-25 16:59:22 -0500

    Finalmente le agarraste a compose

--- ExtractGoddamnedPositions01.jl
+++ ExtractGoddamnedPositions01.jl
@@ -1,8 +1,14 @@
-function ExtractGoddamnedPositions01.jl(mergas::String)
-    turegexp=r"([0-9]{2})_(0-9]{2})"
+function ExtractGoddamnedPositions01(mergas::String)
+    turegexp=r"([0-9]{2})_([0-9]{2})"
     parapa=match(turegexp,mergas)
     perepe=match(turegexp,mergas,parapa.offset+5)
     parte1=int(parapa.captures)
     parte2=int(perepe.captures)
     return (parte1, parte2)
 end
+
+function limpiaarraystring(x::String)
+    result=int(parse(x).args)
+    return result
+end
+    
